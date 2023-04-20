73ba418bcb6deecdf0e3e2d07c10cd15138fe40a
mrobles-dev <miltonjrobles1996@gmail.com>
2023-04-20 02:19:04 -0500

    working on hover effects

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// const images = document.querySelectorAll(".portfolio-images img");
+
+// images.array.forEach(image => {
+//     image.addEventListener("mouseover", () =>{
+//         const id =image.getAttribute("id");
+//         image.setAttribute("title",id)
+//     });
+//     image.addEventListener("mouseout", () =>{
+//         image.removeAttribute("title");
+//     });
+// });
+
+// console.log("ding");
